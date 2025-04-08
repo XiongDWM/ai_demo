da43f008c4470e67d110d42e815c6f02517366e0
XiongDWM <745887012@qq.com>
2025-04-08 16:16:06 +0800

    create ChatApi

--- src/main/java/com/xiongdwm/ai_demo/AiDemoApplication.java
+++ src/main/java/com/xiongdwm/ai_demo/AiDemoApplication.java
@@ -1,5 +1,6 @@
 package com.xiongdwm.ai_demo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AiDemoApplication.class, args);
+		SpringApplication app = new SpringApplication(AiDemoApplication.class);
+		app.setBanner(new CustomBanner());
+		app.setBannerMode(Banner.Mode.CONSOLE);
+		app.run(args);
 	}
 
+	static class CustomBanner implements Banner {
+		@Override
+		public void printBanner(org.springframework.core.env.Environment environment, Class<?> sourceClass, java.io.PrintStream out) {
+			out.println("||-----------------------------------||");
+			out.println("||         AI Demo Application       ||");
+			out.println("||         Version: 0.1              ||");
+			out.println("||         Author: dawei xiong       ||");
+			out.println("||         Date: 2025-04-04          ||");
+			out.println("||-----------------------------------||");
+			out.println("||         Welcome to AI Demo!       ||");
+		}
+	}
 }
---
+++ src/main/java/com/xiongdwm/ai_demo/chat/ChatApi.java
@@ -0,0 +1,5 @@
+package com.xiongdwm.ai_demo.chat;
+
+public class ChatApi {
+    
+}
